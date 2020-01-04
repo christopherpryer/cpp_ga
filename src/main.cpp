f4c9b81b8ca76974e941883fca05a0a2bf575388
Chris Pryer <christophpryer@gmail.com>
2020-01-04 04:48:35 +0000

    slight cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include "population.h"
 #include "utils.h"
+#include <time.h>
 
 int main() {
 
   // initialize params.
-  int initIndividualSize = 6;
-  int initPopSize = 4;
+  int initIndividualSize = 100;
+  int initPopSize = 40000;
+  int generations = 100000;
   Population pop(initIndividualSize, initPopSize, "random");
 
   // initialization tests.
-
   std::vector<Individual> popData = pop.getData();
   int popSize =  popData.size();
   std::vector<int> individualData = popData[0].getData();
@@ -19,7 +20,7 @@ int main() {
   bool hasValidPopulationData = popData.size() == initPopSize;
 
   if ((hasValidIndividualData & hasValidPopulationData) != true) {
-    std::cout << "> test failed: "  << endl;
+    std::cout << "> test failed: " << endl;
     std::cout << "> hasValidIndividualData: " << hasValidIndividualData << endl;
     std::cout << "> hasValidPopulationData: " << hasValidPopulationData << endl;
     return 0;
@@ -30,10 +31,12 @@ int main() {
   std::cout << "individual size: " << individualSize << endl;
 
   // begin optimizaiton.
-  int n = 1;
   std::cout << "running " << n << " generations " << endl;
-  pop.run(n);
 
-  std::cout << "completed simulation" << endl;
+  time_t start = time(0);
+  pop.run(generations);
+  double secondsSinceStart = difftime(time(0), start);
+
+  std::cout << "completed simulation in roughly " << secondsSinceStart << " second(s)" << endl;
   return 0;
 }
