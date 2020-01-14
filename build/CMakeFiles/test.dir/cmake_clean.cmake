2949bc02f9caeb11984c8066aff881f47eb54a52
christopherpryer <christophpryer@gmail.com>
2020-01-14 21:41:15 +0000

    cmake setup (pre-project refactor)

---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/cygdrive/c/users/pryerc/dev/cpp_ga/src/individual.cpp.o"
+  "CMakeFiles/test.dir/cygdrive/c/users/pryerc/dev/cpp_ga/src/main.cpp.o"
+  "CMakeFiles/test.dir/cygdrive/c/users/pryerc/dev/cpp_ga/src/population.cpp.o"
+  "test.pdb"
+  "test.exe"
+  "libtest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
